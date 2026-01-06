90e1701de2b57a5170a8608a42a2d6ed18d85fc9
Ifitkhar Ahammad Sikder <eftyshikder111@gmail.com>
2026-01-07 05:35:26 +0600

    Refactored shimmer logic

--- lib/features/product/presentation/screens/product_screen.dart
+++ lib/features/product/presentation/screens/product_screen.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 import 'package:smart_store_admin/core/utils/widgets/custom_appbar.dart';
-import 'package:smart_store_admin/core/utils/widgets/product_shimmer.dart';
 import 'package:smart_store_admin/core/utils/widgets/product_widgets.dart';
 import 'package:smart_store_admin/features/product/data/controllers/product_controller.dart';
+import 'package:smart_store_admin/features/product/data/models/product_model.dart';
 import 'package:smart_store_admin/features/product/data/services/product_service.dart';
 
 class ProductScreen extends StatefulWidget {
@@ -43,20 +44,31 @@ class _ProductScreenState extends State<ProductScreen> {
                     },
                   ),
                 );*/
-          return productList == null
-              ? ProductShimmer()
-              : productList.isEmpty
-              ? Center(child: Text("No product found"))
-              : Padding(
-                  padding: EdgeInsets.only(top: 10.0, right: 10),
-                  child: GridView.builder(
-                    itemCount: productList.length,
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-                    itemBuilder: (context, index) {
-                      return ProductWidgets(product: productList[index]);
-                    },
-                  ),
-                );
+          return Skeletonizer(
+            enabled: productList == null ? true : false,
+            child: Padding(
+              padding: EdgeInsets.only(top: 10.0, right: 10),
+              child: GridView.builder(
+                itemCount: productList == null ? 8 : productList?.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+                itemBuilder: (context, index) {
+                  // return ProductWidgets(product: productList![index]);
+                  /*if (productController.productList == null) {
+                    return ProductWidgets(product: Product(img: ''));
+                  } else if (productController.productList!.isEmpty) {
+                    return Text("No product fount");
+                  } else {
+                    return ProductWidgets(product: productList![index]);
+                  }*/
+                  return productList == null
+                      ? ProductWidgets(product: Product(img: ''))
+                      : productList.isEmpty
+                      ? Center(child: Text('No prouduct found'))
+                      : ProductWidgets(product: productList[index]);
+                },
+              ),
+            ),
+          );
         },
       ),
     );
